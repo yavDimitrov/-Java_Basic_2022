f49b4cfa466d8034a2512eeb14c840e0e0cb22fa
Yavor Dimitrov <98236929+yavDimitrov@users.noreply.github.com>
2022-07-09 10:24:03 +0300

    Add files via upload
    P-3

---
+++ Lesson_02_ConditionalStatement/Lesson_02_ConditionalStatement/BooleanOperators.java
@@ -0,0 +1,39 @@
+<<<<<<< HEAD
+package Lesson_02_ConditionalStatement;
+
+public class BooleanOperators {
+    public static void main(String[] args) {
+        String name = "Yavor";
+        String name2 = "Dimitrov";
+        int num = 1;
+        int num2 = 5;
+
+        System.out.println(num == num2);
+        System.out.println(num != num2);
+        System.out.println(num >= num2);
+        System.out.println(num <= num2);
+        System.out.println(name.equals(name2));
+
+
+    }
+}
+=======
+package Lesson_02_ConditionalStatement;
+
+public class BooleanOperators {
+    public static void main(String[] args) {
+        String name = "Yavor";
+        String name2 = "Dimitrov";
+        int num = 1;
+        int num2 = 5;
+
+        System.out.println(num == num2);
+        System.out.println(num != num2);
+        System.out.println(num >= num2);
+        System.out.println(num <= num2);
+        System.out.println(name.equals(name2));
+
+
+    }
+}
+>>>>>>> f4f38080b57f0b473462d6a3d4a104f6cf6d0bfa
